e1df846851b21144497bf7c267ee13c7898e6091
Gilles Goffin <ggoffin@free.fr>
2021-10-29 19:10:30 +0200

    lecture ecriture trie

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -4,44 +4,38 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
+	int i = 0;
 	private String filepath;
-	
+
 	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
-	@Override
-	public List<String> GetSymptoms() {
+
+	public ArrayList<String> getSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
-		
 		if (filepath != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+				String symptom = reader.readLine();
+				while (symptom != null) {
+					result.add(symptom);
+					System.out.println("symptom IN : " + symptom);
+					symptom = reader.readLine();
+					i++;
 				}
 				reader.close();
+				System.out.println("------- fin entrée -----" + "Nb enregistrement : " + i + " ------------");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
 		return result;
 	}
-
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomsOut.java
@@ -0,0 +1,52 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class WriteSymptomsOut implements ISymptomWrite {
+
+	// TreeMap est une représentation de données(clé, valeur)
+	// hiérarchique sous forme d'arbre binaire.
+	// Les couples de cette classe sont triés par clé dans l'ordre croissant.
+
+	private TreeMap<String, Integer> symptomMap;
+	private String filepath;
+
+	// changement tableau et fichier en sortie fichierOUT
+	// fichierOUT = "Project02Eclipse/result.out"
+	public WriteSymptomsOut(TreeMap<String, Integer> symptomMap, String filepath) {
+		this.symptomMap = symptomMap;
+		this.filepath = filepath;
+	}
+
+	public void writeSymptomsOut() {
+		File fichierOut = new File(filepath);
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter(fichierOut));
+
+			// interface statique publique Map.Entry<K,V> Une entrée de mappe (paire
+			// clé-valeur).
+			// La méthode Map.entrySet renvoie une vue de collection de la carte
+
+			for (Entry<String, Integer> entry : symptomMap.entrySet()) {
+				writer.write(entry.getKey() + ": " + entry.getValue());
+				System.out.println("symptom out :  " + entry.getKey() + "  nb : " + entry.getValue());
+				writer.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.close();
+				System.out.println("------------------------- fin sortie --------------------------------");
+			} catch (Exception e) {
+			}
+		}
+	}
+}
