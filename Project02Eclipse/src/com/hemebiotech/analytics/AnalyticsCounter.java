0ea57ea0c243a73ef42d5cb3b2adfe1c78756870
Gilles Goffin <ggoffin@free.fr>
2021-10-29 19:05:35 +0200

    correction et traitement des symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,52 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
 
+/**
+ * programme principale AnalyticsCounter
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	// fichier en entrée des symptomms (correction du chemein d'acces du fichier en
+	// entrée)
+	static final String fichierIN = "Project02Eclipse/symptoms.txt";
 
-			line = reader.readLine();	// get another symptom
+	// fichier en sortie des symptoms
+	static final String fichierOUT = "Project02Eclipse/result.out";
+
+	public static void main(String[] args) {
+
+		// La ArrayListclasse est un tableau redimensionnable. éléments peuvent être
+		// ajoutés et supprimés à tout moment.
+		// chargement du fichier en entrée
+
+		ArrayList<String> result = new ArrayList<String>();
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(fichierIN);
+
+		// TreeMap est une représentation de données(clé, valeur) Les couples de cette
+		// classe sont triés par clé dans l'ordre croissant.
+		// changement du fichier en sortie
+
+		TreeMap<String, Integer> symptomMap = new TreeMap<String, Integer>();
+		WriteSymptomsOut writer = new WriteSymptomsOut(symptomMap, fichierOUT);
+
+		// traitement de lecture du fichier en entrée appel de méthode getSymptoms()
+		// La méthode Frequency() de la classe Java Collections est utilisée pour
+		// obtenir le nombre d'éléments dans la collection spécifiée égal à l'objet
+		// spécifié.
+
+		result = reader.getSymptoms();
+
+		if (result != null) {
+			for (String e : result) {
+				if (!symptomMap.containsKey(e)) {
+					symptomMap.put(e, Collections.frequency(result, e));
+				}
+			}
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		// traitement de fichier en sortie avec appel de la méthode writeSymptomsOut()
+		writer.writeSymptomsOut();
 	}
 }
